63df57c32719775ce28c8ffee8a0de2237959bb8
Onkar Kapuskari <on.kaps18@gmail.com>
2026-02-09 23:01:32 +0530

    add chrome storage support for saving user data

--- popup.js
+++ popup.js
@@ -1,8 +1,35 @@
-console.log("Popup js loaded successfully.");
+console.log("StatUp Phase 2 loaded");
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Popup DOM fully loaded");
+    const leetcodeInput = document.getElementById("leetcodeInput");
+    const codeforcesInput = document.getElementById("codeforcesInput");
+    const codechefInput = document.getElementById("codechefInput");
+    const saveBtn = document.getElementById("saveBtn");
+    const status = document.getElementById("status");
 
-    const heading = document.querySelector("h3");
-    heading.style.color = "#0a66c2";
+    chrome.storage.local.get(["userConfig"], (result) => {
+        const config = result.userConfig;
+
+        if (config?.usernames) {
+            leetcodeInput.value = config.usernames.leetcode || "";
+            codeforcesInput.value = config.usernames.codeforces || "";
+            codechefInput.value = config.usernames.codechef || "";
+        }
+    });
+
+    saveBtn.addEventListener("click", () => {
+        const userConfig = {
+            usernames: {
+                leetcode: leetcodeInput.value.trim(),
+                codeforces: codeforcesInput.value.trim(),
+                codechef: codechefInput.value.trim()
+            }
+        };
+
+        chrome.storage.local.set({ userConfig }, () => {
+            console.log("Saved config:", userConfig);
+            status.textContent = "Settings saved!";
+            status.style.color = "green";
+        });
+    });
 });
